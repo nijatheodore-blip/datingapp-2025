4211f0410b85716fb6e37c00c3500ed697d2455e
LAPTOP-NIJA\ntheodore <nija.theodore@pasrons.us>
2025-11-07 16:33:32 -0500

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,8 @@
-using Microsoft.CodeAnalysis.Options;
 using API.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 var builder = WebApplication.CreateBuilder(args);
+
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -10,10 +11,11 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 });
-
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 
